e5dacf7870ed6c8d84d1a152ea81f896fea08180
Yummy312 <kurmushuev.nurzhigit@gmail.com>
2025-01-16 14:27:19 +0600

    Урок 14: Классы и объекты

--- src/Classes_and_Objects.java
+++ src/Classes_and_Objects.java
@@ -1,12 +1,35 @@
+/*********** Урок 14 ************/
+
+
+/*
+   Если перед ключевым словом class стоит ключевое слово public, то это публичный класс.
+   В Java можно иметь только один публичный класс в одном файле, и имя этого класса должно совпадать
+   с именем файла.
+
+   Публичный класс (класс с модификатором public) может быть доступен из любого другого класса или пакета.
+
+ */
 public class Classes_and_Objects {
     // Публичный класс
 
     public static void main(String[] args) {
 
+        // Создание объекта
+        HelperClass1 helperClass1 = new HelperClass1();
+        helperClass1.name="sss";
+        System.out.println(helperClass1.name);
 
 
     }
 
 }
 
+// В одном Java файле может быть сколько угодно обычных классов.
+class HelperClass1 {
+    String name;
+}
+
+class HelperClass2 {
+    String name;
+}
 
